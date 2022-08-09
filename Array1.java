46361f590475d4eea462a63d8b7ab2e8ce443d81
uihalagedara <it20224820@my.sliit.lk>
2022-08-10 00:03:00 +0530

    init

---
+++ Array1.java
@@ -0,0 +1,11 @@
+public class Array1 {
+    public static void main(String[] args) {
+      String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+      //cars[0] = "Opel";
+
+      System.out.println(cars[1]);
+
+      //System.out.println(cars.length);
+    }
+  }
