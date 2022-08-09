5ffa96bf7be456f652a43ea8aef434c1bcd63d7f
uihalagedara <it20224820@my.sliit.lk>
2022-08-10 00:15:03 +0530

    Recursion example added

---
+++ Recursion.java
@@ -0,0 +1,14 @@
+public class Recursion {
+    
+    static int factorial( int n ) {
+        if (n != 0) 
+            return n * factorial(n-1); 
+        else
+            return 1;
+        }
+        public static void main(String[] args) {
+            int number = 4, result;
+            result = factorial(number);
+            System.out.println(number + " factorial = " + result);
+        }
+}
