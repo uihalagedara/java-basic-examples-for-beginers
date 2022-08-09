2df319724456c2986318582e553b858894d6f40f
uihalagedara <it20224820@my.sliit.lk>
2022-08-10 00:08:51 +0530

    Example file added

---
+++ Example.java
@@ -0,0 +1,13 @@
+public class Example {
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 5, c = 6, result;
+    	result = c++ * b % --c;
+    	
+    	System.out.println(result);
+    }
+}
+    
+    
+
