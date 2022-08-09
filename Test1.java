b28b8fbbdf73b96db66cd7d1031326c44c892922
uihalagedara <it20224820@my.sliit.lk>
2022-08-10 00:17:50 +0530

    Test1 file added

---
+++ Test1.java
@@ -0,0 +1,16 @@
+public class Test1 {
+
+    static int sum(int n1, int n2){
+        return n1 + n2;
+    }
+
+    public static void main(String[] args){
+
+       int result =  sum(2,5);
+
+       int result2 = sum(6,10);  
+       System.out.println(result);
+       System.out.println(result2);
+
+    }
+}
