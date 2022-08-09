8e6c115f24cd00869883aefe987bec5c38856e1f
uihalagedara <it20224820@my.sliit.lk>
2022-08-10 00:12:01 +0530

    Example for foreach loop

---
+++ Foreach.java
@@ -0,0 +1,11 @@
+public class Foreach {
+    public static void main(String args[]) {
+        int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int x : nums) {
+            System.out.println(x + " ");
+        }
+
+        //System.out.println(nums[3]);
+        
+    }
+}
