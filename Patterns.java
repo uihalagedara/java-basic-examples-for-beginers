ba112c9d328458055cd3c12c0454c47366548dc2
uihalagedara <it20224820@my.sliit.lk>
2022-08-10 00:13:36 +0530

    Basic start pattern for the beginners

---
+++ Patterns.java
@@ -0,0 +1,10 @@
+public class Patterns{
+    public static void main(String[] args){
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+            System.out.print("* ");
+            }
+            System.out.println("");
+            }
+    }
+}
