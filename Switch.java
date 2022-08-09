2d22dbaf009b675070aa4376d1017e6ff40bf150
uihalagedara <it20224820@my.sliit.lk>
2022-08-10 00:16:39 +0530

    Switch case added

---
+++ Switch.java
@@ -0,0 +1,19 @@
+public class Switch{
+   
+    public static void main(String args[]) {
+        int i = 1;
+        switch (i) {
+            case 1:
+                System.out.println("i is 1");
+                break;
+            case 2:
+                System.out.println("i is 2");
+                break;
+            case 3:
+                System.out.println("i is 3");
+                break;
+            default:
+                System.out.println("i is not 1, 2 or 3");
+        }
+    }
+}
