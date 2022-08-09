e7869501a7557b33a7ec394266ede34a89652c14
uihalagedara <it20224820@my.sliit.lk>
2022-08-10 00:18:23 +0530

    Vehicle file added

---
+++ Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle {
+
+    public void greet(String name){
+        System.out.println("Good morning! " + name);
+    }
+
+    public int sum(int n1, int n2){
+        return n1+n2;
+    }
+    
+}
